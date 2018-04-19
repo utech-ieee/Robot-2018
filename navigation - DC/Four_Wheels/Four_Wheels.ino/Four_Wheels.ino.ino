63b1fe7d634967a1a80e3151dfae02b83ab4364b
Jermaine Coates <jermainecoates@outlook.com>
2018-04-19 06:20:56 -0500

    Add Turn Flag

--- navigation - DC/Four_Wheels/Four_Wheels.ino/Four_Wheels.ino.ino
+++ navigation - DC/Four_Wheels/Four_Wheels.ino/Four_Wheels.ino.ino
@@ -1,70 +1,76 @@
 //#define ENCODER_OPTIMIZE_INTERRUPTS
 #include <Encoder.h>
-#include <PID_v1.h>
+#include <Servo.h>
 
+/***************************************
+ *    COMMUNICATION DECLARATIONS       *
+ ***************************************/
+int numOfTurns;
+String direction;
+/***************************************
+ ***************************************/
+
+/***************************************
+ *         SERVO DECLARATIONS          *
+ ***************************************/
+Servo spinWheel;
+unsigned long previousMillis = 0;
+int wheelPin = 12;
+/***************************************
+ ***************************************/
+
+/***************************************
+ *         MOTOR DECLARATIONS          *
+ ***************************************/
 const uint8_t FRONT_LEFT_MOTOR_IN1 = 24;
 const uint8_t FRONT_LEFT_MOTOR_IN2 = 22;
-const uint8_t FRONT_LEFT_MOTOR_PWM = 11;
+const uint8_t FRONT_LEFT_MOTOR_PWM = 12;
 
-const uint8_t FRONT_RIGHT_MOTOR_IN1 = 4;
-const uint8_t FRONT_RIGHT_MOTOR_IN2 = 3;
-const uint8_t FRONT_RIGHT_MOTOR_PWM = 12;
+const uint8_t FRONT_RIGHT_MOTOR_IN1 = 5;
+const uint8_t FRONT_RIGHT_MOTOR_IN2 = 4;
+const uint8_t FRONT_RIGHT_MOTOR_PWM = 13;
 
-const uint8_t BACK_LEFT_MOTOR_IN1 = 6;
-const uint8_t BACK_LEFT_MOTOR_IN2 = 5;
-const uint8_t BACK_LEFT_MOTOR_PWM = 10;
+const uint8_t BACK_LEFT_MOTOR_IN1 = 7;
+const uint8_t BACK_LEFT_MOTOR_IN2 = 6;
+const uint8_t BACK_LEFT_MOTOR_PWM = 11;
 
-const uint8_t BACK_RIGHT_MOTOR_IN1 = 9;
-const uint8_t BACK_RIGHT_MOTOR_IN2 = 8;
-const uint8_t BACK_RIGHT_MOTOR_PWM = 7;
+const uint8_t BACK_RIGHT_MOTOR_IN1 = 10;
+const uint8_t BACK_RIGHT_MOTOR_IN2 = 9;
+const uint8_t BACK_RIGHT_MOTOR_PWM = 8;
 
+uint8_t motorSpeed = 170; 
+/***************************************
+ ***************************************/
+
+
+/***************************************
+ *       ENCODER DECLARATIONS          *
+ ***************************************/
 Encoder frontleftEncoder(14, 15);
 Encoder frontrightEncoder(16, 17);
 Encoder backleftEncoder(18, 19);
 Encoder backrightEncoder(20, 21);
 
-long frontleftEncoderValue;
-long frontrightEncoderValue;
-long backleftEncoderValue;
-long backrighEncoderValue;
-
-double Setpoint, Input, Output;
-double aggKp=4, aggKi=0.2, aggKd=1;
-double consKp=1, consKi=0.05, consKd=0.25;
-
-PID frontleftPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
-PID frontrightPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
-PID backleftPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
-PID backrightPID(&Input, &Output, &Setpoint,2,5,1, DIRECT);
-
+long positionFrontLeft  = -999;
+long positionFrontRight = -999;
+long positionBackLeft  = -999;
+long positionBackRight = -999;
+/***************************************
+ ***************************************/
+ 
 void setup() 
 {
   Serial.begin(9600);
-  
-  Setpoint = 255;
 
-  //1
-  Input = frontleftEncoder.read();
-  frontleftPID.SetMode(AUTOMATIC);
-
-  //2
-  Input = frontrightEncoder.read();
-  frontrightPID.SetMode(AUTOMATIC);
-
-  //3
-  Input = backleftEncoder.read();
-  backleftPID.SetMode(AUTOMATIC);
-
-  //4
-  Input = backrightEncoder.read();
-  backrightPID.SetMode(AUTOMATIC);
-  
+  /***************************************
+   *               MOTOR                 *
+   ***************************************/
   pinMode(FRONT_LEFT_MOTOR_IN1, OUTPUT);
   pinMode(FRONT_LEFT_MOTOR_IN2, OUTPUT);
   pinMode(FRONT_LEFT_MOTOR_PWM, OUTPUT);
 
   pinMode(FRONT_RIGHT_MOTOR_IN1, OUTPUT);
-  pinMode(FRONT_RIGHT_MOTOR_IN2, LOW);
+  pinMode(FRONT_RIGHT_MOTOR_IN2, OUTPUT);
   pinMode(FRONT_RIGHT_MOTOR_PWM, OUTPUT);
 
   pinMode(BACK_LEFT_MOTOR_IN1, OUTPUT);
@@ -78,11 +84,44 @@ void setup()
 
 void loop()
 {
-  forward(170);
-//  readEncoders(); 
+  split(Serial.readString());
+  if(direction == "FORWARD")
+  {
+    forward(numOfTurns, motorSpeed);
+  }
+  else if (direction == "BACK")
+  {
+    reverse(numOfTurns, motorSpeed);
+  }
+  else if (direction == "LEFT")
+  {
+    left(numOfTurns, motorSpeed);
+  }
+  else if (direction == "RIGHT")
+  {
+    right(numOfTurns, motorSpeed);
+  }
+  else if (direction == "REVERSELEFT")
+  {
+    reverseLeft(numOfTurns, motorSpeed);
+  }
+  else if (direction == "REVERSERIGHT")
+  {
+    reverseRight(numOfTurns, motorSpeed);
+  }
+  else if(direction == "STOP")
+  {
+    stop();
+  }
+  else if(direction == "TURNFLAG")
+  {
+    raiseFlag(numOfTurns);
+  }
+  Serial.write("1");
+  // right(170);
 }
 
-void forward(uint8_t speed)
+void forward(int distance, uint8_t speed)
 {
   digitalWrite(FRONT_LEFT_MOTOR_IN1, HIGH);
   digitalWrite(FRONT_LEFT_MOTOR_IN2, LOW);
@@ -96,10 +135,10 @@ void forward(uint8_t speed)
   digitalWrite(BACK_RIGHT_MOTOR_IN1, HIGH);
   digitalWrite(BACK_RIGHT_MOTOR_IN2, LOW);
   
-  setSpeed(speed);
+  setSpeed(distance, speed);
 }
 
-void reverse(uint8_t speed)
+void reverse(int distance, uint8_t speed)
 {
 
   digitalWrite(FRONT_LEFT_MOTOR_IN1, LOW);
@@ -114,41 +153,75 @@ void reverse(uint8_t speed)
   digitalWrite(BACK_RIGHT_MOTOR_IN1, LOW);
   digitalWrite(BACK_RIGHT_MOTOR_IN2, HIGH);
 
-  setSpeed(speed);
+  setSpeed(distance, speed);
 }
 
-void left(uint8_t speed)
+void left(int distance, uint8_t speed)
 {
   digitalWrite(FRONT_LEFT_MOTOR_IN1, LOW);
   digitalWrite(FRONT_LEFT_MOTOR_IN2, HIGH);
+
+  digitalWrite(FRONT_RIGHT_MOTOR_IN1, LOW);
+  digitalWrite(FRONT_RIGHT_MOTOR_IN2, HIGH);
+
+  digitalWrite(BACK_LEFT_MOTOR_IN1, LOW);
+  digitalWrite(BACK_LEFT_MOTOR_IN2, HIGH);
+
+  digitalWrite(BACK_RIGHT_MOTOR_IN1, HIGH);
+  digitalWrite(BACK_RIGHT_MOTOR_IN2, LOW);
+
+  setSpeed(distance, speed);
+}
+
+void right(int distance, uint8_t speed)
+{
+  digitalWrite(FRONT_LEFT_MOTOR_IN1, HIGH);
+  digitalWrite(FRONT_LEFT_MOTOR_IN2, LOW);
 
   digitalWrite(FRONT_RIGHT_MOTOR_IN1, HIGH);
   digitalWrite(FRONT_RIGHT_MOTOR_IN2, LOW);
 
-  digitalWrite(BACK_LEFT_MOTOR_IN1, LOW);
-  digitalWrite(BACK_LEFT_MOTOR_IN2, HIGH);
-
-  digitalWrite(BACK_RIGHT_MOTOR_IN1, HIGH);
-  digitalWrite(BACK_RIGHT_MOTOR_IN2, LOW);
-
-  setSpeed(speed);
-}
-
-void right(uint8_t speed)
-{
-  digitalWrite(FRONT_LEFT_MOTOR_IN1, HIGH);
-  digitalWrite(FRONT_LEFT_MOTOR_IN2, LOW);
-
-  digitalWrite(FRONT_RIGHT_MOTOR_IN1, LOW);
-  digitalWrite(FRONT_RIGHT_MOTOR_IN2, HIGH);
-
   digitalWrite(BACK_LEFT_MOTOR_IN1, HIGH);
   digitalWrite(BACK_LEFT_MOTOR_IN2, LOW);
 
   digitalWrite(BACK_RIGHT_MOTOR_IN1, LOW);
   digitalWrite(BACK_RIGHT_MOTOR_IN2, HIGH);
 
-  setSpeed(speed);
+  setSpeed(distance, speed);
+}
+
+void reverseLeft(int distance, uint8_t speed)
+{
+  digitalWrite(FRONT_LEFT_MOTOR_IN1, LOW);
+  digitalWrite(FRONT_LEFT_MOTOR_IN2, HIGH);
+  
+  digitalWrite(FRONT_RIGHT_MOTOR_IN1, LOW);
+  digitalWrite(FRONT_RIGHT_MOTOR_IN2, HIGH);
+  
+  digitalWrite(BACK_LEFT_MOTOR_IN1, LOW);
+  digitalWrite(BACK_LEFT_MOTOR_IN2, HIGH);
+
+  digitalWrite(BACK_RIGHT_MOTOR_IN1, HIGH);
+  digitalWrite(BACK_RIGHT_MOTOR_IN2, LOW);
+
+  setSpeed(distance, speed);
+}
+
+void reverseRight(int distance, uint8_t speed)
+{
+  digitalWrite(FRONT_LEFT_MOTOR_IN1, HIGH);
+  digitalWrite(FRONT_LEFT_MOTOR_IN2, LOW);
+  
+  digitalWrite(FRONT_RIGHT_MOTOR_IN1, HIGH);
+  digitalWrite(FRONT_RIGHT_MOTOR_IN2, LOW);
+  
+  digitalWrite(BACK_LEFT_MOTOR_IN1, HIGH);
+  digitalWrite(BACK_LEFT_MOTOR_IN2, LOW);
+
+  digitalWrite(BACK_RIGHT_MOTOR_IN1, LOW);
+  digitalWrite(BACK_RIGHT_MOTOR_IN2, HIGH);
+
+  setSpeed(distance, speed);
 }
 
 void stop()
@@ -165,72 +238,43 @@ void stop()
   digitalWrite(BACK_RIGHT_MOTOR_IN1, LOW);
   digitalWrite(BACK_RIGHT_MOTOR_IN2, HIGH);
 
-  setSpeed(0);
+  setSpeed(0, 0);
+}       
+
+void setSpeed(int distance, uint8_t speed)
+{
+ analogWrite(FRONT_LEFT_MOTOR_PWM, speed);
+ analogWrite(FRONT_RIGHT_MOTOR_PWM, speed);
+ analogWrite(BACK_LEFT_MOTOR_PWM, speed);
+ analogWrite(BACK_RIGHT_MOTOR_PWM, speed);
+}   
+
+void raiseFlag(int turns) 
+{
+  spinWheel.attach(wheelPin);
+  long interval = long(1170*turns);  
+  unsigned long currentMillis = millis();  
+  while (true)
+  {
+    previousMillis = currentMillis;
+    spinWheel.write(180);
+    if (currentMillis - previousMillis >= interval) 
+    {
+      spinWheel.detach();
+      break;
+    }
+  }
 }
 
-//void setSpeed(uint8_t speed)
-//{
-//  Setpoint = speed;
-//
-//  Input = frontleftEncoder.read();
-//  frontleftPID.Compute();
-//  analogWrite(FRONT_LEFT_MOTOR_PWM, Output);
-//
-//  Input = frontrightEncoder.read();
-//  frontrightPID.Compute();
-//  analogWrite(FRONT_RIGHT_MOTOR_PWM, Output);
-//
-//  Input = backleftEncoder.read();
-//  backleftPID.Compute();
-//  analogWrite(BACK_LEFT_MOTOR_PWM, Output);
-//
-//  Input = backrightEncoder.read();
-//  backrightPID.Compute();
-//  analogWrite(BACK_RIGHT_MOTOR_PWM, speed);
-//}       
-
-//void setSpeed(uint8_t speed)
-//{
-//  frontleftEncoderValue = frontleftEncoder.read();
-//  frontrightEncoderValue = frontrightEncoder.read();
-//  backleftEncoderValue = backleftEncoder.read();
-//  backrighEncoderValue = backrightEncoder.read();
-//
-//}
-
- void setSpeed(uint8_t speed)
- {
-   analogWrite(FRONT_LEFT_MOTOR_PWM, speed);
-   analogWrite(FRONT_RIGHT_MOTOR_PWM, speed);
-   analogWrite(BACK_LEFT_MOTOR_PWM, speed);
-   analogWrite(BACK_RIGHT_MOTOR_PWM, speed);
- }   
-
-
-void readEncoders()
+void split(String string)
 {
-  frontleftEncoderValue = frontleftEncoder.read();
-  frontrightEncoderValue = frontrightEncoder.read();
-  backleftEncoderValue = backleftEncoder.read();
-  backrighEncoderValue = backrightEncoder.read();
-
-  Serial.print("frontleftEncoderValue: ");
-  Serial.print(frontleftEncoderValue);
-  Serial.println();
-
-  Serial.print("frontrightEncoderValue: ");
-  Serial.print(frontrightEncoderValue);
-  Serial.println();
-
-  Serial.print("backleftEncoderValue: ");
-  Serial.print(backleftEncoderValue);
-  Serial.println();
-
-  Serial.print("backrighEncoderValue: ");
-  Serial.print(backrighEncoderValue);
-  Serial.println();
-  Serial.println();
-
-  delay(2000);
-
+  for (int i = 0; i < string.length(); i++) 
+  {
+    if (string.substring(i, i+1) == ",") 
+    {
+      direction = string.substring(0, i);
+      numOfTurns = string.substring(i+1).toInt();
+      break;
+    }
+  }
 }
